cc4ec0890165c84c35fba77b9945880568be46bf
benassad zandjibar <benassad16@gmail.com>
2023-08-29 21:02:08 +0200

    portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
 import SocialLinks from "./Components/SocialLinks";
 import About from "./Components/About";
+import Portfolio from "./Components/Portfolio";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar/>
       <Home/>
       <About/>
+      <Portfolio/>
 
       <SocialLinks/>
     </div>
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Htmcss_1 from "../assets/htmcss 1.jpg";
+import Symfony_logo from "../assets/Symfony-logo.jpg";
+
+function Portfolio() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Portfolio
