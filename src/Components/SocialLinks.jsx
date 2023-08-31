1c1ee2efb94ded3da00f4ef25ba5d39b88549a9b
benassad zandjibar <benassad16@gmail.com>
2023-08-31 18:24:49 +0200

    cv

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -13,7 +13,7 @@ function SocialLinks() {
                     LinkdIn <FaLinkedin size={30}/>
                 </>
             ),
-            href: 'www.linkedin.com/in/ben-assad-zandjibar-068024234',
+            href: 'linkedin.com/in/ben-assad-zandjibar-068024234',
             style: 'rounded-tr-md'
         },
         {
@@ -38,10 +38,10 @@ function SocialLinks() {
             id: 4,
             child:(
                 <>
-                    Resume <BsFillPersonLinesFill size={30}/>
+                    Mon CV <BsFillPersonLinesFill size={30}/>
                 </>
             ),
-            href: '',
+            href: '../assets/CV.pdf',
             style: 'rounded-tr-md',
             download: true
         }
